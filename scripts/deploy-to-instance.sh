c54d7bcca7bea50d2ef8d314121fa340b0b627fe
Huda Mohamed <Huda-mohamed1@outlook.com>
2023-09-19 10:07:48 +0100

    deploy process

--- scripts/deploy-to-instance.sh
+++ scripts/deploy-to-instance.sh
@@ -6,8 +6,8 @@ echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-# The instance ip address is 52.12.34.133
+# The instance ip address is 35.178.181.29
 # we should probably avoid hardcoding this, in case
 # the IP changes
-rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@52.12.34.133:/var/acebook/
-ssh -o StrictHostKeyChecking=no ec2-user@52.12.34.133 "sudo systemctl restart acebook"
+rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@35.178.181.29:/var/acebook/
+ssh -o StrictHostKeyChecking=no ec2-user@35.178.181.29 "sudo systemctl restart acebook"
